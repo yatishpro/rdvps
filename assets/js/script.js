183d31c9881cb41985fdbcfa3302af053277c3ad
[Yatish] <[yatishpro@gmail.com]>
2018-11-17 22:46:20 +0530

    "footer silder"
    By Yatsish

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,42 @@
-var services = $('.sider-products');
-    services.owlCarousel({
+var homeIntro = $('.home-intro-products');
+    homeIntro.owlCarousel({
+      items: 4, //10 items above 1000px browser width
+      itemsDesktop: [1000, 2], //5 items between 1000px and 901px
+      itemsDesktopSmall: [900, 2], // betweem 900px and 601px
+      itemsTablet: [600, 1], //2 items between 600 and 0
+      itemsMobile: 1, // itemsMobile disabled - inherit from itemsTablet option
+      slideSpeed: 1000,
+      loop:true,
+      autoPlay: true,
+      autoHeight: true
+  });
+var vpsSlider = $('.vps-slider-products');
+    vpsSlider.owlCarousel({
+      items: 1, //10 items above 1000px browser width
+      itemsDesktop: [1000, 1], //5 items between 1000px and 901px
+      itemsDesktopSmall: [900, 1], // betweem 900px and 601px
+      itemsTablet: [600, 1], //2 items between 600 and 0
+      itemsMobile: 1, // itemsMobile disabled - inherit from itemsTablet option
+      slideSpeed: 1000,
+      loop:true,
+      autoPlay: true,
+      autoHeight: true
+  });
+var dedicatedSlider = $('.dedicated-slider-products');
+    dedicatedSlider.owlCarousel({
+      items: 1, //10 items above 1000px browser width
+      itemsDesktop: [1000, 1], //5 items between 1000px and 901px
+      itemsDesktopSmall: [900, 1], // betweem 900px and 601px
+      itemsTablet: [600, 1], //2 items between 600 and 0
+      itemsMobile: 1, // itemsMobile disabled - inherit from itemsTablet option
+      slideSpeed: 1000,
+      loop:true,
+      autoPlay: true,
+      autoHeight: true
+  });
+
+var procuctsSet = $('.slider-products');
+    procuctsSet.owlCarousel({
       items: 4, //10 items above 1000px browser width
       itemsDesktop: [1000, 2], //5 items between 1000px and 901px
       itemsDesktopSmall: [900, 2], // betweem 900px and 601px
