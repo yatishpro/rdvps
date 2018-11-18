d3fc501b0f99adc40faf4938f295fbf3b7a7913b
[Yatish] <[yatishpro@gmail.com]>
2018-11-18 18:04:01 +0530

    "home slider fix"
    By Yatish

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,18 @@ const productMenu = document.querySelectorAll('.product-menu');
 const mainHeader = document.querySelector('.main-header');
 const mobileButton = document.querySelector('.mobile-menu');
 const menuRef = document.querySelector('.nav-item-rd');
+var homeIntro = $('.home-slider-products');
+    homeIntro.owlCarousel({
+      items: 1, //10 items above 1000px browser width
+      itemsDesktop: [1000, 1], //5 items between 1000px and 901px
+      itemsDesktopSmall: [900, 1], // betweem 900px and 601px
+      itemsTablet: [600, 1], //2 items between 600 and 0
+      itemsMobile: 1, // itemsMobile disabled - inherit from itemsTablet option
+      slideSpeed: 1000,
+      loop:true,
+      autoPlay: true,
+      autoHeight: true
+  });
 var homeIntro = $('.home-intro-products');
     homeIntro.owlCarousel({
       items: 4, //10 items above 1000px browser width
