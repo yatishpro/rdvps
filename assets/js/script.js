420262f87cb0402729455959693274ac92bb50d8
[Yatish] <[yatishpro@gmail.com]>
2018-12-03 18:29:28 +0530

    "product pages design"
    By Yatish

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,18 @@ var homeIntro = $('.home-intro-products');
       autoPlay: true,
       autoHeight: true
   });
+  var procuctsSetVps = $('.vps-landing-products');
+  procuctsSetVps.owlCarousel({
+    items: 4, //10 items above 1000px browser width
+    itemsDesktop: [1000, 2], //5 items between 1000px and 901px
+    itemsDesktopSmall: [900, 2], // betweem 900px and 601px
+    itemsTablet: [600, 1], //2 items between 600 and 0
+    itemsMobile: 1, // itemsMobile disabled - inherit from itemsTablet option
+    slideSpeed: 1000,
+    loop:true,
+    autoPlay: true,
+    autoHeight: true,
+});
 var vpsSlider = $('.vps-slider-products');
     vpsSlider.owlCarousel({
       items: 1, //10 items above 1000px browser width
@@ -53,7 +65,7 @@ var dedicatedSlider = $('.dedicated-slider-products');
       autoPlay: true,
       autoHeight: true
   });
-
+  
 var procuctsSet = $('.slider-products');
     procuctsSet.owlCarousel({
       items: 4, //10 items above 1000px browser width
@@ -66,6 +78,7 @@ var procuctsSet = $('.slider-products');
       autoPlay: true,
       autoHeight: true,
   });
+
 
   window.onload = function(){
       productMenu.forEach(function(iterator){
